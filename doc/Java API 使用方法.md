cafd42faf088987bdf92db98ee2f488e82bf6600
yinchao <877891173@qq.com>
2021-10-10 14:59:35 +0800

    feat: update

--- doc/Java API 使用方法.md
+++ doc/Java API 使用方法.md
@@ -1,13 +1,36 @@
-- [ ] 链表转数组
+- [ ] 链表转 int 数组
 
     ```java
     int[] result = list.toArray(new int[list.size()]);
     ```
 
-- [ ] 数组转链表
+- [ ] int 数组转链表
+
+    ```java
+    Arrays.steam(array).boxed().collect(Collectors.toList())
+    ```
+- [ ] Integer 数组转链表
 
     ```java
     List<Integer> result = new ArrayList<Integer>(Arrays.asList(array));
     ```
 
-- [ ] 
+- [ ] map 输出
+
+    直接标准输出即可(sout)
+
+- [ ] map 判断元素中某个条件是否成立
+
+    ```java
+    // 判断 map 中是否有一个元素的值不为0
+    map.entrySet().stream().anyMatch(item -> item.getValue() != 0);
+    ```
+
+- [ ] 二维基本类型数组排序
+
+    ```java
+    // 以第一维排序
+    Arrays.sort(array,Comparator.comparingInt(o->o[0]));
+    // 以第二维排序
+    Arrays.sort(array,Comparator.comparingInt(o->o[1]));
+    ```
--- test/Main.java
+++ test/Main.java
@@ -1,4 +1,3 @@
-package test;
 
 import java.util.*;
 
@@ -9,5 +8,22 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
+        int[] nums = new int[] { 1, 2, 5, 2, 1, 5 };
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        while (right < nums.length) {
+            if (set.contains(nums[right])) {
+                set.remove(nums[left]);
+                left++;
+            } else {
+                set.add(nums[right]);
+                right++;
+                sum = Math.max(sum, set.stream().mapToInt(Integer::intValue).sum());
+            }
+        }
+        System.out.println(sum);
     }
 }
